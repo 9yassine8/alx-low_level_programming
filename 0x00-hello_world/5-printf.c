509003bb81291d2b6589651a1198372f30b0051c
9yassine8 <elkhttababd@gmailcom>
2023-07-13 20:07:33 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - acts as the starting point for program execution 
+ *
+ * Return - specify the value that the function will return when it is called
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
